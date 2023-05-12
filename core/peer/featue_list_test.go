7fcaec19d721b230b54254253aed5c6197bacf36
jhertz <jh.furtado@campus.fct.unl.pt>
2023-05-12 13:37:59 +0100

    added support for nil as EmptyFeatureList

--- core/peer/featue_list_test.go
+++ core/peer/featue_list_test.go
@@ -6,43 +6,43 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestScores(t *testing.T){
-	testCases := []struct{
-		f1 FeatureList 
-		f2 FeatureList 
+func TestScores(t *testing.T) {
+	testCases := []struct {
+		f1    FeatureList
+		f2    FeatureList
 		score int
 	}{
 		{
-			f1: FeatureList{"a", "b", "c"},
-			f2: FeatureList{"a", "b", "c"},
+			f1:    FeatureList{"a", "b", "c"},
+			f2:    FeatureList{"a", "b", "c"},
 			score: 0b111,
 		},
 		{
-			f1: FeatureList{"a", "b", "c"},
-			f2: FeatureList{"a", "d", "c"},
+			f1:    FeatureList{"a", "b", "c"},
+			f2:    FeatureList{"a", "d", "c"},
 			score: 0b101,
 		},
 		{
-			f1: FeatureList{"a", "b", "c"},
-			f2: FeatureList{"c", "a", "b"},
+			f1:    FeatureList{"a", "b", "c"},
+			f2:    FeatureList{"c", "a", "b"},
 			score: 0b000,
 		},
 		{
-			f1: FeatureList{"a", "b", "c"},
-			f2: FeatureList{"c"},
+			f1:    FeatureList{"a", "b", "c"},
+			f2:    FeatureList{"c"},
 			score: 0b001,
 		},
 		{
-			f1: FeatureList{"a", "b", "c"},
-			f2: FeatureList{"b", "c"},
+			f1:    FeatureList{"a", "b", "c"},
+			f2:    FeatureList{"b", "c"},
 			score: 0b011,
 		},
 		{
-			f1: FeatureList{"a", "b", "c"},
-			f2: FeatureList{"c", "b"},
+			f1:    FeatureList{"a", "b", "c"},
+			f2:    FeatureList{"c", "b"},
 			score: 0b000,
 		},
-	};
+	}
 
 	for _, tcase := range testCases {
 		features1 := tcase.f1
@@ -50,4 +50,26 @@ func TestScores(t *testing.T){
 		require.Equal(t, features2.FeaturesScore(features1), features1.FeaturesScore(features2))
 		require.Equal(t, tcase.score, features1.FeaturesScore(features2))
 	}
+}
+
+func TestEmptyFL(t *testing.T) {
+	aux := FeatureList{
+		"feature-2", "feature-1", "feature-0",
+	}
+
+	e  := emptyFeatureList
+	e2 := FeatureList(nil)
+
+	require.Equal(t, e.Size(), 0)
+	require.Equal(t, e.FeaturesScore(e), 0)
+	require.Equal(t, e.FeaturesScore(aux), 0)
+	require.Equal(t, aux.FeaturesScore(e), 0)
+
+	require.Equal(t, e2.Size(), 0)
+	require.Equal(t, e2.FeaturesScore(e2), 0)
+	require.Equal(t, e2.FeaturesScore(aux), 0)
+	require.Equal(t, aux.FeaturesScore(e2), 0)
+
+	require.Equal(t, aux.Size(), len(aux))
+	require.Equal(t, aux.FeaturesScore(aux), ^(-1 << len(aux)))
 }
--- core/peer/feature_list.go
+++ core/peer/feature_list.go
@@ -1,30 +1,40 @@
 package peer
 
-type FeatureList []string
+type Feature string
+type FeatureList []Feature
+
+var emptyFeatureList FeatureList = nil
 
 func (list FeatureList) FeaturesScore(other FeatureList) int {
+	if &list == &emptyFeatureList || &other == &emptyFeatureList {
+		return 0
+	}
+
 	var smallest, greater FeatureList
 	diff := other.Size() - list.Size()
 	if diff >= 0 {
 		smallest = list
-		greater  = other
+		greater = other
 	} else {
 		smallest = other
-		greater  = list 
-		diff     = -diff
+		greater = list
+		diff = -diff
 	}
 
 	score := 0
-	for i := 0; i < smallest.Size() ; i++{
+	for i := 0; i < smallest.Size(); i++ {
 		bit := 0
-		if smallest[i] == greater[i + diff]{
+		if smallest[i] == greater[i+diff] {
 			bit = 1
 		}
-		score = score << 1 | bit
+		score = score<<1 | bit
 	}
 	return score
 }
 
 func (list FeatureList) Size() int {
+	if &list == &emptyFeatureList {
+		return 0
+	}
 	return len(list)
 }
